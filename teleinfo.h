61a12059b849ca54ae1b2b0882a7c2ebfb6ec63e
neomilium <neomilium@cf92705b-7319-5759-cd79-36cb0f252f98>
2010-11-21 22:04:29 +0000

    Add a clean GPL license.

--- teleinfo.h
+++ teleinfo.h
@@ -1,3 +1,23 @@
+/*-
+* teleinfuse is a FUSE module to access to the "Téléinformation"
+* "Téléinfo" data are transmitted by french electric meters (EDF/ERDF)
+*
+* Copyright (C) 2010 Romuald Conty
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _TELEINFO_H_
 #define _TELEINFO_H_
 
